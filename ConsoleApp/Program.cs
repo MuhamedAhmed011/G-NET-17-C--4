da6bef8a89f7eae1791959cce958cd55d4981c35
MuhamedAhmed011 <aklmuhamedahmed@gmail.com>
2026-02-09 20:18:37 +0200

    Question01

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace ConsoleApp
+﻿using System.Text;
+
+namespace ConsoleApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question01
+            // (A)
+            // In C#, string is immutable,
+            //so each += creates a new string and copies all previous characters
+            // (B)
+            //StringBuilder sb = new StringBuilder();
+
+            //for (int i = 1; i <= 5000; i++)
+            //{
+            //    sb.Append("PROD-");
+            //    sb.Append(i);
+            //    sb.Append(',');
+            //}
+            //string productList = sb.ToString();
+            #endregion
         }
     }
 }
